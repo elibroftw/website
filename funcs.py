757be7d19ce913e91778042afbf1ae9b44de7f5f
elibroftw <xxelibroxx@gmail.com>
2018-08-11 20:58:58 -0400

    update
    Former-commit-id: 309e7138e61c82b47c421daf2c8d267040c7df51
    Former-commit-id: 0f9ac16442415e585b5627f7020d3a5a6cd82497 [formerly bdf6f1cd7523cdba561ad768214e72f6eaec4044]
    Former-commit-id: 4bcf51486d4a3c0a4619021c1817426bc9808c87

--- funcs.py
+++ funcs.py
@@ -1,10 +1,7 @@
 import base64
 import os
-from bs4 import BeautifulSoup
 import requests
-import re
-# from time import time
-# import pypyodbc
+from urllib import parse
 
 
 try:
@@ -20,31 +17,9 @@ SPOTIFY_AUTH_STR = f'{SPOTIFY_CLIENT_ID}:{SPOTIFY_SECRET}'
 SPOTIFY_B64_AUTH_STR = base64.urlsafe_b64encode(SPOTIFY_AUTH_STR.encode()).decode()
 
 
-# def database():
-#     cwd = os.getcwd()
-#     print(cwd)
-#     file = cwd + r'\Information.accdb'
-#     print(file)
-#     pypyodbc.lowercase = False
-#     conn = pypyodbc.connect(
-#         r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};" +
-#         r"Dbq=C:\Users\maste\Documents\Python Projects\Website\Information.accdb;")
-#     cur = conn.cursor()
-#     cur.execute("SELECT ID, First_Name, Last_Name, Pw, Date_of_Birth FROM Information")
-#     while True:
-#         row = cur.fetchone()
-#         if row[1] is None: break
-#         birth_date = row.get('Date_of_Birth').date()  # get date from datetime.datetime
-#         print(f"User {row.get('ID')} is {row.get('First_Name')} {row.get('Last_Name')}")
-#         print(f"User {row.get('ID')} has password {row.get('pw')}")
-#         print(f"User {row.get('ID')} was born on {birth_date}")
-#     cur.close()
-#     conn.close()
-
-
 def get_album_art(artist, track, access_token=None) -> str:
-    """Gets url of album art for the track"""
-    artist, track = urllib.parse.quote_plus(artist), urllib.parse.quote_plus(track)
+    """ Gets url of album art for the track"""
+    artist, track = parse.quote_plus(artist), parse.quote_plus(track)
     if access_token is None:
         header = {'Authorization': 'Basic ' + SPOTIFY_B64_AUTH_STR}
         data = {'grant_type': 'client_credentials'}
