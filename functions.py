7f9bde2933d1b06eda6f080b6a6fae9051034d46
elibroftw <xxelibroxx@gmail.com>
2019-05-29 14:39:25 -0400

    Updated rbhs announcements

--- functions.py
+++ functions.py
@@ -1,18 +1,18 @@
 import base64
+import json
 import os
 import requests
 from urllib import parse
 from PIL import Image
+from environs import Env
 
-try:
-    SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
-    SPOTIFY_SECRET = os.environ['SPOTIFY_SECRET']
-except KeyError:
-    with open('config.txt') as f:
-        lines = f.read().splitlines()
-        SPOTIFY_CLIENT_ID = lines[0][lines[0].index('=') + 2:]
-        SPOTIFY_SECRET = lines[1][lines[1].index('=') + 2:]
+env = Env()
+env.read_env()
 
+
+GOOGLE_API_KEY = os.environ['GOOGLE_API']
+SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
+SPOTIFY_SECRET = os.environ['SPOTIFY_SECRET']
 SPOTIFY_AUTH_STR = f'{SPOTIFY_CLIENT_ID}:{SPOTIFY_SECRET}'
 SPOTIFY_B64_AUTH_STR = base64.urlsafe_b64encode(SPOTIFY_AUTH_STR.encode()).decode()
 
@@ -39,3 +39,11 @@ def img_to_ico(path):
     img.save(f'{directory}/{img_name}.ico')
     # TODO: upload to https://send.firefox.com/ when API comes out
     # TODO: tell user to upload to send.firefox.com if they want to share the file
+
+
+def get_announcements():
+    sheet_id = '1Re7s1xqGNJH89iUTha-nTeerJXN-61C3mvLW6dqttks'
+    url = f'https://sheets.googleapis.com/v4/spreadsheets/{sheet_id}/values/%20FilteredForm!$A$4:$YY'
+    r = requests.get(url, params={'key': GOOGLE_API_KEY})
+    announcements = json.loads(r.text)['values']
+    return announcements  # [ [TITLE, DESC], [TITLE, DESC] ]
