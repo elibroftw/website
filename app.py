36eeb40c5a47425a92090ea98b336c92c6c744e0
Elijah Lopez <elijahllopezz@gmail.com>
2019-07-19 00:19:13 -0400

    Added project
    Former-commit-id: 84562f767bf228688f3b1db36a4ed805a6e8d5d6
    Former-commit-id: a40a75ed94ced29e4b5a4991c25f314708b87e60 [formerly 3cf638a47e15e1fec2ffbf0ebdea21b67560206d]
    Former-commit-id: 73b3bd7b684917ead7b7eb43db1f6ff027c7b719

--- app.py
+++ app.py
@@ -198,4 +198,4 @@ def to_ico():
 
 if __name__ == '__main__':
     if not os.path.exists('static/MP3Editor'): os.mkdir('static/MP3Editor')
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='', port=5000)
