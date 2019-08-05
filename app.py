187a2534a35ef23c6bb5d265a48c91bf22f21f1a
Elijah Lopez <elijahllopezz@gmail.com>
2019-08-05 12:35:14 -0400

    Update app.py
    Former-commit-id: fe340f43b31ff7881c0277401efc6dcb67b2963e
    Former-commit-id: 9d3953803fe089b1ab0ac9f5311721e18fb545d0 [formerly 5b9bc629b884adbb1b486da211191606ca5c7966]
    Former-commit-id: af453ab46f72c2791232d849367b6035be96c891

--- app.py
+++ app.py
@@ -24,14 +24,7 @@ def add_header(response):
 
 @app.errorhandler(404)
 def page_not_found(_):
-    # TODO: change cookie name
-    # theme_on = request.cookies['website-theme']
     return render_template('404.html'), 404
-
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/')
