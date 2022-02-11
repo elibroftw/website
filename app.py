7f85fbf1059063083f3991f616166a765251da7a
Elijah Lopez <elijahllopezz@gmail.com>
2022-02-10 20:26:49 -0500

    Fix

--- app.py
+++ app.py
@@ -413,5 +413,5 @@ if __name__ == '__main__':
     def test_page():
         return render_template('test.html')
 
-    app.run(debug=True, host='', port=5001)
+    app.run(debug=True, host='[::1]', port=5000)
     # socketio.run(app, debug=True, host='', port=5000)
