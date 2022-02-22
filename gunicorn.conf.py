64894705ea77d5c447f971cea9620b748d55e722
Elijah Lopez <elijahllopezz@gmail.com>
2022-02-21 19:16:12 -0500

    Performance

---
+++ gunicorn.conf.py
@@ -0,0 +1,12 @@
+import multiprocessing
+
+workers = multiprocessing.cpu_count() * 2 + 1
+preload_app = True
+pidfile = 'gunicorn.pid'
+wsgi_app = 'app:app'
+capture_output = True
+errorlog = 'gunicorn.log'
+# umask = 0o664
+# daemon = True
+# threads = 2
+# bind = ['unix:gunicorn.sock']
