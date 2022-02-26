753d43bd6d1fadfd5ba7fb284e5807987f0942c8
Elijah Lopez <elijahllopezz@gmail.com>
2022-02-25 19:34:56 -0500

    Update MC Screenshots

--- app.py
+++ app.py
@@ -274,7 +274,7 @@ def music_caster():
         return redirect(f'music-caster:{args}')
     # second var is width
     images = [
-        ('https://github.com/elibroftw/music-caster/blob/master/resources/SC-Main.png?raw=true', 'Main'),
+        ('https://github.com/elibroftw/music-caster/blob/master/resources/SC-Main.jpg?raw=true', 'Main'),
         ('https://github.com/elibroftw/music-caster/blob/master/resources/SC-Mini.png?raw=true', 'Mini-Mode'),
         ('https://github.com/elibroftw/music-caster/blob/master/resources/SC-Tray.png?raw=true', 'Tray'),
         ('https://github.com/elibroftw/music-caster/blob/master/resources/SC-Web.png?raw=true', 'Web GUI')
